f802437b9999b99406e9fbb7fe6b515a9e84ab4a
artvezhl <artvezhl@gmail.com>
2022-10-07 19:13:45 +0600

    init commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './App.module.scss';
+
+function App() {
+  return (
+    <div className={s.App}>
+      <h1>Trood test App</h1>
+      <div>ProgressBar Component</div>
+    </div>
+  );
+}
+
+export default App;
